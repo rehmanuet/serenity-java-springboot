c7fe3fddaf27c0464b4e95611aef404786867392
Abdur.Ashraf <abdur.ashraf@rabobank.nl>
2024-02-12 12:38:16 +0100

    first commit

---
+++ src/test/java/Runner/Junit5CucumberRunner.java
@@ -0,0 +1,11 @@
+package Runner;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class Junit5CucumberRunner {
+}
---
+++ src/test/java/Runner/Junit5CucumberRunner2.java
@@ -0,0 +1,13 @@
+package Runner;
+
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ExcludeTags(value = {"ignore", "red"})
+public class Junit5CucumberRunner2 {
+}
