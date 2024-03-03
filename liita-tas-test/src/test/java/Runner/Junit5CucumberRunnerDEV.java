68df6fa7ff98a1eb485870bede2010c86c0d0db7
Abdur Rehman Ashraf <abdurrehmanashraf@arambp.local>
2024-03-03 20:46:54 +0100

    added reporting

--- liita-tas-test/src/test/java/Runner/Junit5CucumberRunnerDEV.java
+++ liita-tas-test/src/test/java/Runner/Junit5CucumberRunnerDEV.java
@@ -8,11 +8,13 @@ import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @RunWith(CucumberSerenityRunner.class)
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.liita.tas.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporter")
 @ExcludeTags(value = {"ignore", "qa"})
 public class Junit5CucumberRunnerDEV {
 }
