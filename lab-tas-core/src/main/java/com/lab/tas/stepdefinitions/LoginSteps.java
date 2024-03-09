08bccf6567e7e4e528cce5510f53ad485becef35
Abdur Rehman Ashraf <abdurrehmanashraf@arambp.local>
2024-03-09 01:26:59 +0100

    add native driver

--- lab-tas-core/src/main/java/com/lab/tas/stepdefinitions/LoginSteps.java
+++ lab-tas-core/src/main/java/com/lab/tas/stepdefinitions/LoginSteps.java
@@ -5,32 +5,27 @@ import com.lab.tas.configuration.TestContextConfiguration;
 import com.lab.tas.context.ScenarioContext;
 import com.lab.tas.stepdefinitions.actions.LoginPage;
 import io.cucumber.java.en.Given;
+import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationMethodRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(SerenityRunner.class)
 @SpringBootTest(classes = TestContextConfiguration.class)
-/*
-        Remove later
-        @ContextConfiguration(classes = TestContextConfiguration.class)
-     */
+
 public class LoginSteps extends UIInteractionSteps {
     @Steps
     SpringConfiguration configuration;
 
     @Autowired
     ScenarioContext context;
-    /*
-        Remove later
-        @ClassRule
-        public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-     */
+
     @Rule
     public SpringIntegrationMethodRule springIntegrationMethodRule =
             new SpringIntegrationMethodRule();
